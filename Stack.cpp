fed0d0a1666d01c0d03587bfa4540663106fa3b5
Booddda <ab.galal254@gmail.com>
2024-10-05 14:57:48 +0300

    Copy constructor

--- Stack.cpp
+++ Stack.cpp
@@ -8,16 +8,22 @@ private:
     int size;
     int *st;
     int tos;
+    static int count;
+
 public:
+    friend void viewContent(Stack s); 
+    Stack (Stack &z);
     Stack(int s = 10)
     {
         tos = 0;
         size = s;
+        count++;
         st = new int [size];
     }
     ~Stack()
     {
         delete [] st;
+        count--;
     }
     void push(int n)
     {
@@ -44,14 +50,42 @@ public:
         }
         return retVal;
     };
+    static int getCounter()
+    {
+        return count;
+        
+    }
 };
+int Stack::count = 0;
+// copy constructor implementation
+Stack :: Stack (Stack &x)
+    {
+        tos = x.tos;
+        size = x.size;
+        st = new int [size]; // new allocation instead of copying to avoid changing the main array content
+        for(int i = 0; i < tos; i++)
+        {
+            st[i] = x.st[i];
+        }
+        count++; 
+    };
+
+void viewContent(Stack s)
+{
+    int t = s.tos; 
+    while(t != 0)
+    {
+        cout << s.st[--t] << endl;
+    }
+}
 
 int main()
 {
     Stack s1(5);
     s1.push(5);
     s1.push(7);
-    cout << s1.pop() << " " << s1.pop() << " " << endl;
+    viewContent(s1);
+    cout << s1.pop() << " " << s1.pop() << " " << Stack::getCounter() << endl;
 
     return 0;
 }
